6bea385723c31665051b3a55414300ff4811e0f9
NURUL SYUHADA <syuhada@kptm.edu.my>
2020-12-15 10:29:02 +0800

    Controller:add TrainingController

---
+++ database/seeders/TrainingsTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class TrainingsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('trainings')->insert([
+            'title' => 'Training Laravel 7 Days @ KPTM',
+            'description' => 'Training Laravel 7 Days @ KPTM is ongoing',
+            'trainer' => 'Tarmizi Sanusi'
+
+        ]);
+        //
+    }
+}
