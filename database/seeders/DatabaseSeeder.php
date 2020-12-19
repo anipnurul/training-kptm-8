b332cfa2bdfd61ff0512c142479bd83e5632189d
NURUL SYUHADA <syuhada@kptm.edu.my>
2020-12-19 22:57:54 +0800

    Add:new table Products

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(TrainingsTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
     }
 }
